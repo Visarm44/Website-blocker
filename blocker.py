a4907b6849224522a7d772eeef4a31772a4fdca2
Visarm44 <118958222+Visarm44@users.noreply.github.com>
2025-11-24 11:11:59 +0100

    Changed file names

--- blocker.py
+++ blocker.py
@@ -1,17 +1,41 @@
-import time
-from classes import Blocker
+from datetime import datetime
 
-host_path = "/etc/hosts"
-redirect = "127.0.0.1"
-website_list = ["www.facebook.com", "facebook.com", "www.youtube.com"]
-hours_window = (8, 17)
+class Blocker:
+    def __init__(self, host_path, redirect, website_list):
+        self.host_path = host_path
+        self.redirect = redirect
+        self.website_list = website_list
 
-blocker = Blocker(host_path, redirect, website_list)
-
-while True:
-    if blocker.block_condition(hours_window):
-        blocker.block()
-    else:
-        blocker.unblock()
+    def block_condition(self, time_window=(0, 24), t=None): # Checks if the current time is in the specified time window
+        if t == None:
+            t = datetime.now().hour
+        
+        if time_window[0] <=  t < time_window[1]:
+            return True
+        else:
+            return False
+        
+    def block(self):
+        with open(self.host_path, "r") as f:
+           host_content = f.read()
     
-    time.sleep(5)
+        websites_to_add = []
+        for website in self.website_list:
+            if website not in host_content:
+                websites_to_add.append(f"\n{self.redirect} {website}")
+
+        if to_add:
+            with open(self.host_path, "a") as f:
+                f.write("".join(to_add))
+
+    def unblock(self):
+        with open(self.host_path, "r+") as f:
+            lines = f.readlines()
+
+            f.seek(0)
+
+            for line in lines:
+                if not any(website in line for website in self.website_list):
+                    f.write(line)
+        
+            f.truncate()
---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+from blocker import Blocker
+
+host_path = "/etc/hosts"
+redirect = "127.0.0.1"
+website_list = ["www.facebook.com", "facebook.com", "www.youtube.com"]
+hours_window = (8, 17)
+
+blocker = Blocker(host_path, redirect, website_list)
+
+while True:
+    if blocker.block_condition(hours_window):
+        blocker.block()
+    else:
+        blocker.unblock()
+    
+    time.sleep(5)
